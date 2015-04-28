2f55f44b0189f48b3c0ca87803bad0229b51193f
mansoo0609 <mansoo0609@gmail.com>
2015-04-28 03:50:46 -0400

    sql updates

--- include/setup.sql
+++ include/setup.sql
@@ -6,7 +6,8 @@ create table studentlist (
 	localaddress varchar(50),
 	major varchar(50),
 	school enum('CSOM', 'A&S', 'Lynch', 'CSON'),
-	subjects varchar(100)
+	subjects varchar(100),
+	availability varchar(100)
 );
 
 create table tutorlist (
@@ -18,5 +19,8 @@ create table tutorlist (
 	major varchar(50),
 	grade enum('Freshmen', 'Sophomore', 'Junior', 'Senior'),
 	school enum('CSOM', 'A&S', 'Lynch', 'CSON'),
-	subjects varchar(100)
+	subjects varchar(100),
+	availability varchar(100),
+	rating enum ('*', '**', '***', '****', '*****'),
+	ratingcounter int ()
 );
