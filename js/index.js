2bd7819f8911bf3c8b3ba3326ca12bd5919e7d1f
mansoo0609 <mansoo0609@gmail.com>
2015-04-28 10:18:51 -0400

    JS stuff

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
-function validedatetype() {
-	var type = '';
-	var len = document.frmOne.accounttype.length;
+function validatetype() {
+/*  	var type = '';
+ 	var len = document.frmOne.accounttype.length;
 	for (i = 0; i < len; i++) {
-		if ( document.frmOne.payment[i].checked ) {
-			type = document.frmOne.payment[i].value;
+		if ( document.frmOne.accounttype[i].checked ) {
+			type = document.frmOne.accounttype[i].value;
 			break;
 		}
 	}
 	
 	if ( type == '' ) {
 		document.getElementById("accounttypeerror").innerHTML  = "<font color='red'Please select an account type.</font>";
-	} else {
-		document.getElementById("accounttypeerror").innerHTML  = "";
-	}
+		return false;
+	} */
+	
+	document.getElementById("accounttypeerror").innerHTML  = "";
+	return false;
+}
+
+function validate() {
+	return false;
 }
