e2d71c0d326832c5f0a510ece68ffbc724ba1755
mansoo0609 <mansoo0609@gmail.com>
2015-04-28 14:28:24 -0400

    student sign up validations

--- js/signup.js
+++ js/signup.js
@@ -1,20 +1,33 @@
 function validatestudentsignup() {
 	var namevalid = validatename();
 	var gendervalid = validategender();
-	if ( gendervalid ) {
+	var emailvalid = validateemail();
+	var passwordvalid = validatepassword();
+	var addressvalid = validateaddress();
+	var majorvalid = validatemajor();
+	var schoolvalid = validateschool();
+	var subjectsvalid = validatesubjects();
+	var availibilityvalid = validateavailability();
+	if ( namevalid && gendervalid && emailvalid && passwordvalid && addressvalid && majorvalid && schoolvalid && subjectsvalid && availibilityvalid ) {
 		return true;
 	}
 	return false;
 }
 
 function validatename() {
-		var name = document.getElementById("enteredname").value;
-		if  ( name.trim() == "" ){
-			document.getElementById("nameerror").innerHTML  = "<font color='red'>Please input a name.</font>";
-			return false;
-		}
-		document.getElementById("nameerror").innerHTML  = '';
-		return true;
+	
+	var thename = document.getElementById("enteredname").value;
+	var tomatch = /^\w{2,}\ \w{2,}$/;   // accepts the form: "Firstname Lastname" with minimum 2 letters each
+	
+	if ( !tomatch.test(thename) ) {
+		var errorrpt = document.getElementById("nameerror");
+		errorrpt.style.color = "red";
+		errorrpt.innerHTML = "Please enter your name as \"Firstname Lastname\", 2 letters minimum.";
+		return false;
+	}
+	var errorrpt = document.getElementById("nameerror");
+	errorrpt.innerHTML = "";
+	return true;
 }
 
  
@@ -30,4 +43,97 @@ function validategender() {
 		document.getElementById("gendererror").innerHTML  = "<font color='red'>Please select a gender.</font>";
 		return false;
 	}
+}
+
+function validateemail() {
+	
+	var theemail = document.getElementById("enteredemail").value;
+	var tomatch = /^(\w|\.){1,}\@\w{1,}\.(com|net|org|edu)$/;   // accepts the form: "email@bc.site" where "site" can be com, net, org, or edu
+	
+	if ( !tomatch.test(theemail) ) {
+		var errorrpt = document.getElementById("emailerror");
+		errorrpt.style.color = "red";
+		errorrpt.innerHTML = "Please enter an email in the form \"email@host.domain\".";
+		return false;
+	}
+	
+	var errorrpt=document.getElementById("emailerror");
+	errorrpt.innerHTML = "";
+	return true;
+}
+
+function validatepassword() {
+	
+	var password1 = document.getElementById("password1").value;
+	var password2 = document.getElementById("password2").value;
+	var tomatch = /^\w{5,}$/;   // accepts passwords that are minimum 5 characters long
+	
+	if ( !tomatch.test(password1) ) {
+		var errorrpt = document.getElementById("password1error");
+		errorrpt.style.color = "red";
+		errorrpt.innerHTML = "Please enter a password with at least 5 characters.";
+		return false;
+	} else if ( password2 != password1 ) {
+		var errorrpt = document.getElementById("password1error");
+		errorrpt.innerHTML = "";
+		var errorrpt = document.getElementById("password2error");
+		errorrpt.style.color = "red";
+		errorrpt.innerHTML = "Please doublecheck your passwords - they do not match.";
+		return false;
+	}
+	var errorrpt = document.getElementById("password1error");
+	errorrpt.innerHTML = "";
+	var errorrpt = document.getElementById("password2error");
+	errorrpt.innerHTML = "";
+	return true;
+}
+
+function validateaddress() {
+	var address = document.getElementById("enteredaddress").value;
+	if  ( address.trim() == "" ){
+		document.getElementById("addresserror").innerHTML  = "<font color='red'>Please input an address.</font>";
+		return false;
+	}
+	document.getElementById("addresserror").innerHTML  = '';
+	return true;
+}
+
+function validatemajor() {
+	var major = document.getElementById("enteredmajor").value;
+	if  ( major.trim() == "" ){
+		document.getElementById("majorerror").innerHTML  = "<font color='red'>Please input a major.</font>";
+		return false;
+	}
+	document.getElementById("majorerror").innerHTML  = '';
+	return true;
+}
+
+function validateschool() {
+	var school = document.getElementById("enteredschool").selectedIndex;
+	if  ( school == "0"  ){
+		document.getElementById("schoolerror").innerHTML  = "<font color='red'>Please choose a school.</font>";
+		return false;
+	}
+	document.getElementById("schoolerror").innerHTML  = '';
+	return true;
+}
+
+function validatesubjects() {
+	var subjects = document.getElementById("enteredsubjects").value;
+	if  ( subjects.trim() == "" ){
+		document.getElementById("subjectserror").innerHTML  = "<font color='red'>Please input some subjects.</font>";
+		return false;
+	}
+	document.getElementById("subjectserror").innerHTML  = '';
+	return true;
+}
+
+function validateavailability() {
+	var availability = document.getElementById("enteredavail").value;
+	if  ( availability.trim() == "" ){
+		document.getElementById("availerror").innerHTML  = "<font color='red'>Please input availability times.</font>";
+		return false;
+	}
+	document.getElementById("availerror").innerHTML  = '';
+	return true;
 }
