d901c6258f40dd7698f065126e5bacb7b9861ef2
mansoo0609 <mansoo0609@gmail.com>
2015-04-28 09:29:35 -0400

    index

--- index.php
+++ index.php
@@ -13,8 +13,10 @@
 
 <?php
 
-	if (isset($_GET['getstarted'])) {
+	if (isset( $_GET['login']) ) {
 		loginpage(); 
+	} else if ( isset($_GET['signup']) ) {
+		signuppage();
 	}
 	else {
 		welcomepage();
@@ -34,7 +36,8 @@
 		
 		<h2> We proudly announce the launch of a new website where BC tutors could meet BC students easily and efficiently! </h2>
 		
-		<input type = "submit" name = "getstarted" value = "Login with your ID">
+		<input type = "submit" name = "login" value = "Login with your ID"> <br>
+		<input type = "submit" name = "signup" value = "Make a New Account">
 	</form> 
 	
 <?php
@@ -42,8 +45,8 @@
 	
 	function loginpage() {
 ?>	
-		<form method = "post">
 		<fieldset> 
+		<form method = "post">
 			<h3>Are You a Student or a Tutor?</h3>
 			Student <input type = "radio" name = "type" value = "student">
 			Tutor <input type = "radio" name = "type" value = "tutor"> <br>
@@ -51,9 +54,27 @@
 			Enter your username and password <br>
 			<input type = "text" name = "user" value = ""> <br>
 			<input type = "password" name = "pass" value = ""> <br>
-	
+		</form>
 		</fieldset>
-		</form> 
+<?php
+	}
+	
+	function signuppage() {
+?>
+		<fieldset>
+			<form method = "post" onsubmit="return false;" action="">
+				Select Account Type: <input type='radio' name='accounttype' value='student' /> Student
+									<input type='radio' name='accounttype' value='tutor' /> Tutor
+									<div id="accounttypeerror"></div>
+				Enter Your Name: <input type='text' name='name' id='enteredname' />
+				<div id="nameerror"></div>
+				Enter Your Email: <input type='text' name='email' id='enteredemail' />
+				<div id="emailerror"></div>
+				Choose your Password: <input type='password' name='password1' id='password1' /> <br>
+				Confirm your Password: <input type='password' name='password2' id='password2' />
+				<div id="passworderror"></div>
+			</form>
+		</fieldset>	
 <?php
 	}
 ?>
