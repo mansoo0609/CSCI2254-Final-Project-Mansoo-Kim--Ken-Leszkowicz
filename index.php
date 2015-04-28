ca9aca06b68cc5c563247bf329f0f58d2e11a803
mansoo0609 <mansoo0609@gmail.com>
2015-04-28 02:25:14 -0400

    First Upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<title>Final Project</title>
+	<!--<script type="text/javascript" src="js/index.js"></script>-->
+</head>
+
+<body>
+
+<?php
+
+?>
+
+</body>
+
+</html>
