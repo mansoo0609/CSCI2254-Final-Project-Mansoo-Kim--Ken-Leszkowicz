a7ffcd54d95d51fc0bb304abd8fc36174235dc64
mansoo0609 <mansoo0609@gmail.com>
2015-04-28 03:43:24 -0400

    dbconn added

--- include/setup.sql
+++ include/setup.sql
@@ -8,3 +8,15 @@ create table studentlist (
 	school enum('CSOM', 'A&S', 'Lynch', 'CSON'),
 	subjects varchar(100)
 );
+
+create table tutorlist (
+	name varchar(50),
+	email varchar(50),
+	password char(40),
+	registrationdate datetime,
+	localaddress varchar(50),
+	major varchar(50),
+	grade enum('Freshmen', 'Sophomore', 'Junior', 'Senior'),
+	school enum('CSOM', 'A&S', 'Lynch', 'CSON'),
+	subjects varchar(100)
+);
