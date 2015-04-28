ac4c79f9d01d863311d4273e5b7eaabab643b721
leszkowi@bc.edu <leszkowi@bc.edu>
2015-04-28 02:54:38 -0400

    Basic addition of fieldset and inputs

--- index.php
+++ index.php
@@ -10,6 +10,18 @@
 
 <body>
 
+<h1> Welcome! </h1>
+
+<fieldset> 
+	<legend> Are you a Student or a Tutor? </legend>
+	
+	I am a... <br>
+	
+	<input type = "submit" name = "student" value = "Student">
+	<input type = "submit" name = "tutor" value = "Tutor">
+</fieldset>
+		
+
 <?php
 
 ?>
