0840c92ca0721e908e2b6db61f54f3f769c7303d
mansoo0609 <mansoo0609@gmail.com>
2015-04-28 09:51:04 -0400

    signup update

--- signup.php
+++ signup.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/signup.js"></script>
+</head>
+
+<body>
+</body>
+
+</html>
+
+
+
+
 <?php	
 	function signuppage() {
 ?>
