d447108523c334f6d0a70bdec5ff12fd2cdb35f4
mansoo0609 <mansoo0609@gmail.com>
2015-04-28 03:05:12 -0400

    Index
    adding some stuff

--- index.php
+++ index.php
@@ -4,21 +4,23 @@
 
 <head>
 	<meta charset="utf-8" />
-	<title>Final Project</title>
+	<title>Boston College Tutoring Service</title>
 	<!--<script type="text/javascript" src="js/index.js"></script>-->
 </head>
 
 <body>
 
-<h1> Welcome! </h1>
+<h1> Welcome to BC's Tutoring Service! </h1>
 
 <fieldset> 
+
 	<legend> Are you a Student or a Tutor? </legend>
 	
 	I am a... <br>
 	
 	<input type = "submit" name = "student" value = "Student">
 	<input type = "submit" name = "tutor" value = "Tutor">
+	
 </fieldset>
 		
 
