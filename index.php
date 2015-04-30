bee318c24517d5ea78d11faab26221cac2e39dd0
mansoo0609 <mansoo0609@gmail.com>
2015-04-30 00:37:47 -0400

    Started login page and validation

--- index.php
+++ index.php
@@ -12,13 +12,27 @@
 <body>
 
 <?php
-		include "login.php";
 		include "signup.php";
 		
 		if ( isset($_GET['login']) ) {
 			loginpage();
 		} else if ( isset($_GET['signup']) ) {
 			selectaccounttype();
+			if ( isset($_GET['accounttype']) ) {
+				$accounttype = $_GET['accounttype'];
+				if ( $accounttype == 'student' ) {
+					signuppagestudent();
+				} else {
+					signuppagetutor();
+				}
+			}
+		} else if ( isset($_GET['accounttype']) ) {
+				$accounttype = $_GET['accounttype'];
+				if ( $accounttype == 'student' ) {
+					signuppagestudent();
+				} else {
+					signuppagetutor();
+				}
 		} else {
 			welcomepage();
 		}
@@ -38,7 +52,7 @@
 		
 		<h2> We proudly announce the launch of a new website where BC tutors could meet BC students easily and efficiently! </h2>
 		
-		<input type = "submit" name = "login" value = "Login with your ID" > <br>
+		<input type = "submit" name = "login" value = "Login with your Email" > <br>
 		<input type = "submit" name = "signup" value = "Make a New Account" >
 	</form> 
 	
@@ -48,13 +62,29 @@
 	function selectaccounttype() {
 ?>
 		<fieldset>
-			<form method='get' onsubmit='return validatetype();' action='signup.php' name='selecttype'>
+			<form method='get' onsubmit='return validatetype();'>
 				<label class="floatleft">Select Account Type:</label>
 					<input type="radio" name="accounttype" value="student" /> Student
 					<input type="radio" name="accounttype" value="tutor" /> Tutor
 					<label class="floatright" id="accounttypeerror">  </label> <br>
 				<input type='submit' name='typeselected' value='Make an Account' />
 			</form>
+		</fieldset>
+<?php
+	}
+	
+	function loginpage() {
+?>	
+		<fieldset> 
+		<form method = "post" action ='login.php'>
+			<h3>Are You a Student or a Tutor?</h3>
+			Student <input type = "radio" name = "type" value = "student">
+			Tutor <input type = "radio" name = "type" value = "tutor"> <br><br>
+			
+			<label style="width: 100px; display: inline-block;">Email: </label><input type = "text" name = "email" value = ""> <br>
+			<label style="width: 100px; display: inline-block;">Password: </label><input type = "password" name = "pass" value = ""> <br>
+			<input type='submit' name='login' value='Sign In' />
+		</form>
 		</fieldset>
 <?php
 	}
--- login.php
+++ login.php
@@ -1,19 +1,65 @@
-<?php
-	
-	function loginpage() {
-?>	
-		<fieldset> 
-		<form method = "post">
-			<h3>Are You a Student or a Tutor?</h3>
-			Student <input type = "radio" name = "type" value = "student">
-			Tutor <input type = "radio" name = "type" value = "tutor"> <br>
+<!DOCTYPE html>
 
-			Enter your username and password <br>
-			<input type = "text" name = "user" value = ""> <br>
-			<input type = "password" name = "pass" value = ""> <br>
-		</form>
-		</fieldset>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<title>User Home Page</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
 <?php
+
+	include ('database/dbconn.php');
+	
+	$type = $_POST['type'];
+	
+	if ( $type == 'student' ) {
+		$list = 'studentlist';
+	} else {
+		$list = 'tutorlist';
 	}
 	
+	$newemail = $_POST['email'];
+	$password = $_POST['password'];
+	//checkemail( $list, $newemail );
+	checkpassword ( $list, $password, $newemail );
+	
+?>
+
+</body>
+
+</html>
+
+<?php
+
+	function checkemail( $list, $newemail ) {
+		
+		$dbc = connectToDB( 'kimbxn' ) ; 
+		 
+		$query = "Select * from $list where email = '$newemail' ";
+		$result = performQuery($dbc, $query); 
+		
+		if ( mysqli_num_rows($result) == 0 ) {
+			disconnectFromDB($dbc, $result);
+			return false;
+		} else {
+			disconnectFromDB($dbc, $result);
+			return true;
+		}
+	}
+	
+	function checkpassword ( $list, $password, $newemail ) {
+		
+		$dbc = connectToDB( 'kimbxn' ) ;
+		
+		$password = sha1($password);
+		
+		$query = "Select email from $list where password = '$password' ";
+		$result = performQuery($dbc, $query);
+		
+		
+	}
 ?>
